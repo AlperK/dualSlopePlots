cfe5d032ab08f85f120980f96be396bac2397d6a
AlperK <alperkilic93@gmail.com>
2022-05-13 14:58:21 -0400

    .

--- DemodulatorPlot.py
+++ DemodulatorPlot.py
@@ -18,7 +18,7 @@ def sine_model(x, amp, a, freq, phi, offset):
     return amp * a * (np.sin(2*np.pi*freq*(x+90) + phi)) + offset
 
 
-p0 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('PhaseCalibration'),
+p0 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('PhaseCalibration'),
                    Path('1kHz_100mV_500mV_phase.csv'))
 pha0 = []
 vol0 = []
@@ -28,7 +28,7 @@ with open(p0, newline='') as csv_file:
         pha0.append(int(row[0]))
         vol0.append(float(row[1])*1e3)
 
-p1 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('PhaseCalibration'),
+p1 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('PhaseCalibration'),
                    Path('1kHz_200mV_500mV_phase.csv'))
 pha1 = []
 vol1 = []
@@ -38,7 +38,7 @@ with open(p1, newline='') as csv_file:
         pha1.append(int(row[0]))
         vol1.append(float(row[1])*1e3)
 
-p2 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('PhaseCalibration'),
+p2 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('PhaseCalibration'),
                    Path('1kHz_300mV_500mV_phase.csv'))
 pha2 = []
 vol2 = []
@@ -48,7 +48,7 @@ with open(p2, newline='') as csv_file:
         pha2.append(int(row[0]))
         vol2.append(float(row[1])*1e3)
 
-p3 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('PhaseCalibration'),
+p3 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('PhaseCalibration'),
                    Path('1kHz_400mV_500mV_phase.csv'))
 pha3 = []
 vol3 = []
@@ -58,7 +58,7 @@ with open(p3, newline='') as csv_file:
         pha3.append(int(row[0]))
         vol3.append(float(row[1])*1e3)
 
-a0 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('AmplitudeCalibration'), Path('1kHz_amplitude.csv'))
+a0 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('AmplitudeCalibration'), Path('1kHz_amplitude.csv'))
 amp0 = []
 a_vol0 = []
 with open(a0, newline='') as csv_file:
@@ -67,7 +67,7 @@ with open(a0, newline='') as csv_file:
         amp0.append(int(row[0]))
         a_vol0.append(float(row[1])*1e3)
 
-# a1 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('AmplitudeCalibration'), Path('4kHz_amplitude.csv'))
+# a1 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('AmplitudeCalibration'), Path('4kHz_amplitude.csv'))
 # amp1 = []
 # a_vol1 = []
 # with open(a1, newline='') as csv_file:
@@ -76,7 +76,7 @@ with open(a0, newline='') as csv_file:
 #         amp1.append(int(row[0]))
 #         a_vol1.append(float(row[1])*1e3)
 #
-# a2 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('AmplitudeCalibration'), Path('7kHz_amplitude.csv'))
+# a2 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('AmplitudeCalibration'), Path('7kHz_amplitude.csv'))
 # amp2 = []
 # a_vol2 = []
 # with open(a2, newline='') as csv_file:
@@ -85,7 +85,7 @@ with open(a0, newline='') as csv_file:
 #         amp2.append(int(row[0]))
 #         a_vol2.append(float(row[1])*1e3)
 #
-# a3 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-2'), Path('AmplitudeCalibration'), Path('10kHz_amplitude.csv'))
+# a3 = Path.joinpath(Path('2022-05-12'), Path('Demodulator-1'), Path('AmplitudeCalibration'), Path('10kHz_amplitude.csv'))
 # amp3 = []
 # a_vol3 = []
 # with open(a3, newline='') as csv_file:
@@ -143,8 +143,9 @@ result4 = tmodel.fit(vol3[45:135], params, x=pha0[45:135])
 lmodel = Model(line)
 
 params = lmodel.make_params(a=1, b=0)
-params['b'].vary = False
-
+# params['b'].vary = False
+amplitudeFit = lmodel.fit(a_vol0, params, method='leastsq', x=amp0)
+print(amplitudeFit.fit_report())
 # result5 = lmodel.fit(a_vol0, params, x=amp0)
 # result6 = lmodel.fit(a_vol1, params, x=amp1)
 # result7 = lmodel.fit(a_vol2, params, x=amp2)
@@ -162,61 +163,61 @@ smodel = Model(sine_model)
 # params['phi'].vary = False
 # params['offset'].vary = False
 
-params = smodel.make_params(amp=0.1, a=302.5, freq=2.77e-3, phi=0, offset=-1)
-params['amp'].vary = False
-params['a'].vary = False
-params['freq'].vary = False
-params['phi'].vary = False
-# params['offset'].vary = False
-sine1 = smodel.fit(vol0[45:135], params, method='leastsq', x=pha0[45:135])
+# params = smodel.make_params(amp=0.1, a=302.5, freq=2.77e-3, phi=0, offset=-1)
+# params['amp'].vary = False
+# params['a'].vary = False
+# params['freq'].vary = False
+# params['phi'].vary = False
+# # params['offset'].vary = False
+# sine1 = smodel.fit(vol0[45:135], params, method='leastsq', x=pha0[45:135])
+#
+# params = smodel.make_params(amp=0.2, a=302.5, freq=2.77e-3, phi=0, offset=-1)
+# params['amp'].vary = False
+# params['a'].vary = False
+# params['freq'].vary = False
+# params['phi'].vary = False
+# # params['offset'].vary = False
+# sine2 = smodel.fit(vol1[45:135], params, method='leastsq', x=pha0[45:135])
+#
+# params = smodel.make_params(amp=0.3, a=302.5, freq=2.77e-3, phi=0, offset=-1)
+# params['amp'].vary = False
+# params['a'].vary = False
+# params['freq'].vary = False
+# params['phi'].vary = False
+# # params['offset'].vary = False
+# sine3 = smodel.fit(vol2[45:135], params, method='leastsq', x=pha0[45:135])
+#
+# params = smodel.make_params(amp=0.4, a=301, freq=2.77e-3, phi=0, offset=-1)
+# params['amp'].vary = False
+# params['a'].vary = False
+# params['freq'].vary = False
+# params['phi'].vary = False
+# # params['offset'].vary = False
+# sine4 = smodel.fit(vol3[45:135], params, method='leastsq', x=pha0[45:135])
 
-params = smodel.make_params(amp=0.2, a=302.5, freq=2.77e-3, phi=0, offset=-1)
-params['amp'].vary = False
-params['a'].vary = False
-params['freq'].vary = False
-params['phi'].vary = False
-# params['offset'].vary = False
-sine2 = smodel.fit(vol1[45:135], params, method='leastsq', x=pha0[45:135])
-
-params = smodel.make_params(amp=0.3, a=302.5, freq=2.77e-3, phi=0, offset=-1)
-params['amp'].vary = False
-params['a'].vary = False
-params['freq'].vary = False
-params['phi'].vary = False
-# params['offset'].vary = False
-sine3 = smodel.fit(vol2[45:135], params, method='leastsq', x=pha0[45:135])
-
-params = smodel.make_params(amp=0.4, a=301, freq=2.77e-3, phi=0, offset=-1)
-params['amp'].vary = False
-params['a'].vary = False
-params['freq'].vary = False
-params['phi'].vary = False
-# params['offset'].vary = False
-sine4 = smodel.fit(vol3[45:135], params, method='leastsq', x=pha0[45:135])
-
-print(sine1.fit_report())
-print(sine2.fit_report())
-print(sine3.fit_report())
-print(sine4.fit_report())
-
-plt.figure(figsize=(6, 6))
-plt.scatter(pha0, vol0, s=15, label=r'$V_{sig} = 100 mV$')
-plt.scatter(pha0, vol1, s=15, label=r'$V_{sig} = 200 mV$')
-plt.scatter(pha0, vol2, s=15, label=r'$V_{sig} = 300 mV$')
-plt.scatter(pha0, vol3, s=15, label=r'$V_{sig} = 400 mV$')
-
-plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.1, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
-            label='SINE-FIT-0.1')
-plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.2, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
-            label='SINE-FIT-0.2')
-plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.3, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
-            label='SINE-FIT-0.3')
-plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.4, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
-            label='SINE-FIT-0.4')
-plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.5, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
-            label='SINE-FIT-0.5')
-plt.legend()
-plt.tight_layout()
+# print(sine1.fit_report())
+# print(sine2.fit_report())
+# print(sine3.fit_report())
+# print(sine4.fit_report())
+#
+# plt.figure(figsize=(6, 6))
+# plt.scatter(pha0, vol0, s=15, label=r'$V_{sig} = 100 mV$')
+# plt.scatter(pha0, vol1, s=15, label=r'$V_{sig} = 200 mV$')
+# plt.scatter(pha0, vol2, s=15, label=r'$V_{sig} = 300 mV$')
+# plt.scatter(pha0, vol3, s=15, label=r'$V_{sig} = 400 mV$')
+#
+# plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.1, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
+#             label='SINE-FIT-0.1')
+# plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.2, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
+#             label='SINE-FIT-0.2')
+# plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.3, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
+#             label='SINE-FIT-0.3')
+# plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.4, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
+#             label='SINE-FIT-0.4')
+# plt.scatter(pha0, sine_model(np.array([pha0]), amp=0.5, a=303, freq=0.00277, phi=0, offset=-0.3), s=1, c='k',
+#             label='SINE-FIT-0.5')
+# plt.legend()
+# plt.tight_layout()
 
 
 # Phase Plot
@@ -246,24 +247,24 @@ plt.tight_layout()
 # plt.grid()
 
 # Amplitude Plot
-# plt.figure(figsize=(6, 6))
-# plt.scatter(amp0, a_vol0, s=5, label=r'1 kHz')
+plt.figure(figsize=(6, 6))
+plt.scatter(amp0, a_vol0, s=5, label=r'1 kHz')
 # plt.scatter(amp1, a_vol1, s=5, label=r'4 kHz')
 # plt.scatter(amp2, a_vol2, s=5, label=r'7 kHz')
 # plt.scatter(amp3, a_vol3, s=5, label=r'10 kHz')
 
-# plt.scatter(amp0, line(np.array([amp0]), 0.2063, 0), s=1, c='k', label='FIT-1kHz')
+plt.scatter(amp0, line(np.array([amp0]), 0.3011, 0.826), s=1, c='k', label='FIT-1kHz')
 # plt.scatter(amp0, line(np.array([amp0]), 0.2063 * (201/209), 0), s=1, c='k', label='FIT-1kHz')
 # plt.scatter(amp1, line(np.array([amp1]), 0.2063 * (201/209), 0), s=1, c='k', label='FIT-4kHz')
 # plt.scatter(amp2, line(np.array([amp2]), 0.2063 * (201/209), 0), s=1, c='k', label='FIT-7kHz')
 # plt.scatter(amp3, line(np.array([amp3]), 0.2063 * (201/209), 0), s=1, c='k', label='FIT-10kHz')
 
-# plt.title('Amplitude calibration')
-# plt.legend()
-# plt.xticks([0, 500, 1000, 1500, 2000])
-# plt.xlabel('Input amplitude ($V_{pp}$)')
-# plt.yticks([0, 25, 50, 75, 100])
-# plt.ylabel('Output amplitude ($V_{dc}$)')
-# plt.grid()
-# plt.tight_layout()
+plt.title('Amplitude calibration')
+plt.legend()
+plt.xticks([0, 100, 200, 350, 400, 500])
+plt.xlabel('Input amplitude ($V_{pp}$)')
+plt.yticks([0, 50, 100, 150])
+plt.ylabel('Output amplitude ($V_{dc}$)')
+plt.grid()
+plt.tight_layout()
 plt.show()
